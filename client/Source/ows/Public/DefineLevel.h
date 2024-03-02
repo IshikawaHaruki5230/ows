714ea0a9ad74a7f66ea8d4e9bd120293257c733f
itusi <itusi.free@gmail.com>
2024-03-03 01:42:16 +0900

    コンテンツ追加

---
+++ client/Source/ows/Public/DefineLevel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DefineLevel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OWS_API ADefineLevel : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
